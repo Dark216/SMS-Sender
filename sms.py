65824e868a41f91b5a832eafee3b361a88cae1e5
Sandip Nayak <46400343+SandipNayak@users.noreply.github.com>
2020-05-11 22:18:48 +0530

    updated

--- sms.py
+++ sms.py
@@ -4,8 +4,8 @@ import json
 def send_sms(number,message):
     url = "https://www.sms4india.com/api/v1/sendCampaign"
     parameters={
-        'apikey':"YGMTHBTWODO1I1JASBXLVMA61BLVJAEW",
-        'secret':"8PZAMP5V4T50ZIK3",
+        'apikey':"",
+        'secret':"",
         'usetype':"stage",
         'phone': number,
         'message':message,
@@ -15,4 +15,4 @@ def send_sms(number,message):
     dic = response.json
     print(dic)
     
-send_sms("8895820008","Binayak Panda stay safe from corona virus,take preventive measures and obey lockdown. Stay safe and stay healthy")
+send_sms("","")
